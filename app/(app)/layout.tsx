4ad0533a9b78690efbae2a3656e940a041c8bd6b
Anjana Paul <anjanapaul0614@gmail.com>
2025-03-25 17:38:18 +0400

    credit buton update, #3

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -36,7 +36,7 @@ export default async function AppLayout({
               {/* <ThemeSelector /> */}
               {/* <p>fffffff</p> */}
               <div>
-              <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">5</button>
+              {/* <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">5</button> */}
               <CreditButton/>
               </div>
               <ModeSwitcher />
--- components/credit-button.tsx
+++ components/credit-button.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/registry/new-york-v4/ui/button";
 
 export  function CreditButton() {
   return (
-    <Button>
+    <Button className="font-bold py-4 px-4 rounded border-4 border-red-500">
         5
     </Button>
   )
