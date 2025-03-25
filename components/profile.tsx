ee5f6dfc0dc147660b51d0481cfc3f15ccff9345
Anjana Paul <anjanapaul0614@gmail.com>
2025-03-25 17:53:12 +0400

    avatarfallback, #3

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,9 +1,10 @@
-import { Avatar, AvatarImage } from '@/registry/new-york-v4/ui/avatar'
+import { Avatar, AvatarFallback, AvatarImage } from '@/registry/new-york-v4/ui/avatar'
 
 export  function Profile() {
   return (
     <Avatar className='border-4 border-red-500'>
         <AvatarImage src='https://github.com/shadcn.png'/>
+        <AvatarFallback>CN</AvatarFallback>
     </Avatar>
   )
 }
