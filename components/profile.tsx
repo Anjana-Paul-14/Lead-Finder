868a98c8cab4b83c173298a70307ac3d2af5168e
Anjana Paul <anjanapaul0614@gmail.com>
2025-03-26 16:57:09 +0400

    updation, #3

--- components/profile.tsx
+++ components/profile.tsx
@@ -25,30 +25,3 @@ export  function Profile() {
   )
 }
 
-
-
-// "use client"
-
-// import { Avatar, AvatarFallback, AvatarImage } from '@/registry/new-york-v4/ui/avatar';
-// import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/registry/new-york-v4/ui/dropdown-menu';
-
-// export function Profile() {
-//   return (
-//     <DropdownMenu>
-//       <DropdownMenuTrigger asChild>
-//         <Avatar className="w-12 h-12 cursor-pointer">
-//           <AvatarImage src="https://github.com/shadcn.png" className="rounded-full" />
-//           <AvatarFallback>CN</AvatarFallback>
-//         </Avatar>
-//       </DropdownMenuTrigger>
-//       <DropdownMenuContent className="w-40">
-//         <DropdownMenuItem onClick={() => console.log("Settings Clicked")}>
-//           Settings
-//         </DropdownMenuItem>
-//         <DropdownMenuItem onClick={() => console.log("Logout Clicked")}>
-//           Logout
-//         </DropdownMenuItem>
-//       </DropdownMenuContent>
-//     </DropdownMenu>
-//   );
-// }
