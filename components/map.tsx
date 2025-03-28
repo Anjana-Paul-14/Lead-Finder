d97fc603419e34f15cd8f4d4f5c30cf248cac25d
Anjana Paul <anjanapaul0614@gmail.com>
2025-03-28 15:11:29 +0400

    added search bar, #9

--- components/map.tsx
+++ components/map.tsx
@@ -1,11 +1,20 @@
 'use client'
 import { useEffect, useState } from 'react';
 import {LoadScript, GoogleMap, Marker} from '@react-google-maps/api'
+import { Command, CommandDialog , CommandInput,
+    CommandList,
+    CommandEmpty,
+    CommandGroup,
+    CommandItem,
+    CommandShortcut,
+    CommandSeparator} from '@/registry/new-york-v4/ui/command';
+import { Button } from '@/registry/new-york-v4/ui/button';
+
 
 
 const containerStyle = {
     width: "100%",
-    height: "80%",
+    height: "60vh",
   };
   
 //   const center = {
@@ -36,6 +45,9 @@ export const Map = () => {
   }, []);
 
   return (
+    <div className='flex flex-col items-center w-full h-full border-4 border-amber-300'>
+    {/* <> */}
+    <div className='w-full flex justify-center pt-4'>
     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_MAPS_API_KEY as string}>
 
         {currentLocation ? (
@@ -47,5 +59,29 @@ export const Map = () => {
         )
         }
     </LoadScript>
+    </div>
+
+    <div className='flex flex-row items-center py-8'>
+        <div className='w-100 '>
+        <Command>
+        <CommandInput placeholder="Type a command or search..." />
+        {/* <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+    <CommandGroup heading="Suggestions">
+      <CommandItem>Calendar</CommandItem>
+      <CommandItem>Search Emoji</CommandItem>
+      <CommandItem>Calculator</CommandItem>
+    </CommandGroup>
+        </CommandList> */}
+        </Command>
+        </div>
+<div className='pl-4'>
+    <Button>
+Search
+    </Button>
+</div>
+    </div>
+    {/* </> */}
+     </div>
   )
 }
