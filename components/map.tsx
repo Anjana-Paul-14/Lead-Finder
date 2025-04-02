ef3d97a4a0e18a954c62376f5a1a5c5003f0e32c
Anjana Paul <anjanapaul0614@gmail.com>
2025-04-02 14:37:40 +0400

    updation, #9

--- components/map.tsx
+++ components/map.tsx
@@ -106,7 +106,12 @@ export const Map = () => {
         value={searchQuery}
         onValueChange={setSearchQuery} />
         <CommandList>
-        
+        <CommandEmpty>No results found.</CommandEmpty>
+    <CommandGroup heading="Suggestions">
+      <CommandItem>Calendar</CommandItem>
+      <CommandItem>Search Emoji</CommandItem>
+      <CommandItem>Calculator</CommandItem>
+    </CommandGroup>
         </CommandList>
         </Command>
         </div>
