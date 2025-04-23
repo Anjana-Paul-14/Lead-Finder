9da3b3425c83e5eab6703a0185c5c955a7b1c0b2
Anjana Paul <anjanapaul0614@gmail.com>
2025-04-23 15:42:02 +0530

    implement CreditContext and CreditProvider for credit state management #13

--- context/CreditContext.tsx
+++ context/CreditContext.tsx
@@ -1,9 +1,31 @@
 'use client'
 
-import React from 'react'
 
-export const CreditContext = () => {
+import React, {useState,createContext, useContext,} from 'react'
+
+type CreditContextType = {
+  credits: number
+  decrementCredits: () => void
+}
+
+const CreditContext = createContext<CreditContextType>({
+  credits: 5,
+  decrementCredits: () => {},
+})
+
+export function CreditProvider({ children }: { children: React.ReactNode }) {
+  const [credits, setCredits] = useState(5)
+
+  const decrementCredits = () => {
+    setCredits(prev => Math.max(prev - 1, 0)) // Prevent negative numbers
+  }
+
+
   return (
-    <div>CreditContext</div>
+    <CreditContext.Provider value={{ credits, decrementCredits }}>
+      {children}
+    </CreditContext.Provider>
   )
 }
+
+export const useCredits = () => useContext(CreditContext)
