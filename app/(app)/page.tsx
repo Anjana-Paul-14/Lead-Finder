d2e3b3b609bd76fac2254357a91696ad392bc311
Anjana Paul <anjanapaul0614@gmail.com>
2025-06-06 12:57:18 +0530

    create login page #18 updated login page

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -3,7 +3,6 @@ import {
   Lexend as FontSans,
   Newsreader as FontSerif,
 } from "next/font/google"
-
 import { cn } from "@/lib/utils"
 import { LoginForm } from "@/components/login-form"
 import LoginImg from '../../public/login_img.jpg'
@@ -25,20 +24,10 @@ export default function LoginPage() {
       )}
       style={{ height:'100vh'}}
     >
-      {/* <div className="w-full max-w-sm md:max-w-3xl">
-        <LoginForm />
-      </div> */}
       <div className="theme-login-one w-full max-w-sm md:max-w-3xl">
         <LoginForm imageUrl={LoginImg.src} />
+      </div>
 
-        {/* <LoginForm imageUrl="https://images.unsplash.com/photo-1482872376051-5ce74ebf0908?q=80&w=3050&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" /> */}
-      </div>
-      {/* <div className="theme-login-two w-full max-w-sm md:max-w-3xl">
-        <LoginForm imageUrl="https://images.unsplash.com/photo-1498758536662-35b82cd15e29?q=80&w=3088&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-      </div>
-      <div className="theme-login-three w-full max-w-sm md:max-w-3xl">
-        <LoginForm imageUrl="https://images.unsplash.com/photo-1536147116438-62679a5e01f2?q=80&w=2688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-      </div> */}
     </div>
   )
 }
