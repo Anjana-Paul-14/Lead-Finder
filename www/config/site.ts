412bd3ff09c800e0e90cda3fcc3190ea4b079800
Anjana Paul <anjanapaul0614@gmail.com>
2025-06-09 13:12:24 +0530

    Add: Create site configuration in site.ts create login page #18

---
+++ www/config/site.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  name: "ShadCN Location",
+  url: "https://shadcn-location.vercel.app/",
+  description: "Welcome to my site!",
+  ogImage: "https://example.com/og-image.jpg",
+}
