0341169da18086822d1508cc2fb24c01e36e44bb
Anjana Paul <anjanapaul0614@gmail.com>
2025-06-09 13:12:48 +0530

    Fix: Comment out ogImage property in site configuration create login page #18

--- www/config/site.ts
+++ www/config/site.ts
@@ -2,5 +2,5 @@ export const siteConfig = {
   name: "ShadCN Location",
   url: "https://shadcn-location.vercel.app/",
   description: "Welcome to my site!",
-  ogImage: "https://example.com/og-image.jpg",
+//   ogImage: "https://example.com/og-image.jpg",
 }
