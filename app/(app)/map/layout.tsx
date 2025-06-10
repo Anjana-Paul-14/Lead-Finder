ee8fb90eb0e3a39b2876d7454f41d0387b483eed
Anjana Paul <anjanapaul0614@gmail.com>
2025-06-10 22:51:12 +0530

    fix: remove unnecessary comment in AppLayout component create login page #18

--- app/(app)/map/layout.tsx
+++ app/(app)/map/layout.tsx
@@ -38,6 +38,7 @@ export default async function AppLayout({
               <CreditButton/>
               </div>
               <ModeSwitcher />
+              {/* //eee */}
               <Profile/>
             </div>
           </div>
