387bdb5211c581ea42c024ba2d873efedbea806a
Anjana Paul <anjanapaul0614@gmail.com>
2025-06-18 18:38:11 +0530

    refactor: remove unnecessary width specification from header in Map component

--- app/(app)/map/layout.tsx
+++ app/(app)/map/layout.tsx
@@ -25,7 +25,7 @@ export default async function AppLayout({
     <SidebarProvider defaultOpen={defaultOpen} >
       <AppSidebar />
       <SidebarInset>
-        <header className="bg-background sticky inset-x-0 top-0 isolate z-10 flex shrink-0 items-center gap-2 w-full border-b border-4 border-amber-300">
+        <header className="bg-background sticky inset-x-0 top-0 isolate z-10 flex shrink-0 items-center gap-2 border-b border-4 border-amber-300">
           <div className="flex h-14 w-full items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1.5" />
             <Separator
