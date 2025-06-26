ece31d00bca7e11ecb428b2ce770afbce5dc04e1
Anjana Paul <anjanapaul0614@gmail.com>
2025-06-26 17:46:22 +0530

    refactor: uncomment User model schema and export statement in User.ts

--- models/User.ts
+++ models/User.ts
@@ -7,3 +7,16 @@
 // })
 
 // export default mongoose.models.User || mongoose.model("User", UserSchema)
+
+
+import mongoose from "mongoose"
+
+const UserSchema = new mongoose.Schema ({
+  name: { type: String, required: true },
+ email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+})
+
+
+const User = mongoose.models.User || mongoose.model("User", UserSchema)
+export default User;
