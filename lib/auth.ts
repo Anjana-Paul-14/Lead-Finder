f65afa0f55fc909732cbe34b9133a9f4cbd3b3af
Anjana Paul <anjanapaul0614@gmail.com>
2025-07-12 10:52:33 +0530

    refactor: comment out unused authentication code in auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,21 +1,21 @@
-// lib/auth.ts
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+// // lib/auth.ts
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async session({ session, token }: any) {
-      session.user.id = token.sub;
-      return session;
-    },
-  },
-};
+// export const authOptions = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//   ],
+//   secret: process.env.NEXTAUTH_SECRET,
+//   callbacks: {
+//     async session({ session, token }: any) {
+//       session.user.id = token.sub;
+//       return session;
+//     },
+//   },
+// };
 
-export default NextAuth(authOptions);
+// export default NextAuth(authOptions);
