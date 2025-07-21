1ba352281efe6dc355955af6e4d2a0b23abb6d30
Anjana Paul <anjanapaul0614@gmail.com>
2025-07-21 22:47:43 +0530

    msg11

--- components/nav-header.tsx
+++ components/nav-header.tsx
@@ -40,7 +40,7 @@ export function NavHeader() {
         {/* git  */}
         {/* commit */}
         {/* green */}
-        color
+        {/* color */}
         to 
         show
         consistency
