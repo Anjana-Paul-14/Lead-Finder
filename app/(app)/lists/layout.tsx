372bf57697fc54bce2b5b185b0005a5fa18fb38c
Anjana Paul <anjanapaul0614@gmail.com>
2026-01-14 14:27:14 +0530

    list layout

--- app/(app)/lists/layout.tsx
+++ app/(app)/lists/layout.tsx
@@ -24,15 +24,18 @@ export default async function AppLayout({
     <SidebarProvider defaultOpen={defaultOpen} >
       <AppSidebar />
       <SidebarInset>
-        <header className="bg-background sticky inset-x-0 top-0 isolate z-10 flex shrink-0 items-center gap-2 border-b border-4">
-          <div className="flex h-14 w-full items-center gap-2 px-4">
+        {/* <header className="bg-background sticky inset-x-0 top-0 isolate z-10 flex shrink-0 items-center gap-2 border-b border-4"> */}
+          <header className="bg-background sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          {/* <div className="flex h-14 w-full items-center gap-2 px-4"> */}
+          <div className="flex w-full items-center gap-2">
             <SidebarTrigger className="-ml-1.5" />
             <Separator
               orientation="vertical"
               className="mr-2 data-[orientation=vertical]:h-4"
             />
             <NavHeader />
-            <div className="ml-auto flex items-center gap-2 border-4">
+            {/* <div className="ml-auto flex items-center gap-2 border-4"> */}
+            <div className="ml-auto flex items-center gap-4">
               <div>
              
               </div>
