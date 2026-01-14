4a0a82c712cf24ba6fe22465681e5999bd6790bd
Anjana Paul <anjanapaul0614@gmail.com>
2026-01-14 15:02:18 +0530

    page-title-image

--- app/config/site.ts
+++ app/config/site.ts
@@ -3,5 +3,5 @@ export const siteConfig = {
   // url: "https://shadcn-location.vercel.app/", // Local development URL
   url: "https://lead-find.netlify.app/", // Local development URL
   description: "A modern UI for location-based services using Next.js and ShadCN UI.",
-  ogImage: "https://via.placeholder.com/1200x630.png?text=OG+Image", // Placeholder Open Graph image
+  // ogImage: "https://via.placeholder.com/1200x630.png?text=OG+Image", 
 }
